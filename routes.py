ad615da877846a2c74b19d3e29d6067a1d0d4120
Ashish Kumar <116388479+Ashish2537@users.noreply.github.com>
2025-09-02 13:52:50 +0530

    Update routes.py

--- routes.py
+++ routes.py
@@ -159,7 +159,6 @@ def delete_blog(blog_id):
     flash("Blog deleted successfully.")
     return redirect(url_for('view_all_blogs'))
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
